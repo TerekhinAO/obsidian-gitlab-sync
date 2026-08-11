17fcd0f54dee23bb6a4ead62f5eabeaf3bf39908
Aleksandr Terekhin <a.o.terekhin@tbank.ru>
2026-08-11 21:17:51 +0300

    chore: release 1.0.14

--- src/utils.ts
+++ src/utils.ts
@@ -38,8 +38,10 @@ export async function copyToClipboard(text: string) {
     const textarea = document.createElement("textarea");
     textarea.value = text;
     textarea.setAttribute("readonly", "");
-    textarea.style.position = "absolute";
-    textarea.style.left = "-9999px";
+    textarea.setCssStyles({
+      position: "absolute",
+      left: "-9999px",
+    });
     document.body.appendChild(textarea);
 
     textarea.select();
